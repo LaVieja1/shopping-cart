334f51264de577577c9ef5ad4a8c6d99633127f1
Santi Cano <canosantiago404@gmail.com>
2021-10-28 13:56:37 -0300

    added data

---
+++ src/data/categories.js
@@ -0,0 +1,9 @@
+const categories = [
+    { id: "processors", name: "Procesadores" },
+    { id: "memory", name: "Memoria" },
+    { id: "motherboards", name: "Motherboards" },
+    { id: "video-cards", name: "Placa de video" },
+    { id: "chassis", name: "Torres" },
+  ];
+  
+  export default categories;
